3aa4c0d54939ef5e28921c076e64256b731c71db
Amir Ghorbani <82311179+AmirGhorbaniDev@users.noreply.github.com>
2023-08-15 15:00:00 +0300

    Add reminder system with database support

--- bot.py
+++ bot.py
@@ -1,12 +1,14 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 import sqlite3
-from datetime import datetime
+from datetime import datetime, timedelta
+import asyncio
 
 # Bot setup
 intents = discord.Intents.default()
-intents.members = True  # Required to detect member joins
-intents.messages = True  # Required to log messages
+intents.members = True  # Enable member-related events
+intents.messages = True  # Enable message-related events
+intents.reactions = True  # Enable reaction-related events
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 # Database setup
@@ -24,12 +26,22 @@ CREATE TABLE IF NOT EXISTS messages (
     timestamp TEXT
 )
 """)
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS reminders (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id TEXT,
+    username TEXT,
+    reminder_text TEXT,
+    remind_at TEXT
+)
+""")
 conn.commit()
 
 
 @bot.event
 async def on_ready():
     print(f"{bot.user.name} is online!")
+    check_reminders.start()
 
 
 # Welcome message for new members
@@ -65,30 +77,61 @@ async def botinfo(ctx):
     await ctx.send(embed=embed)
 
 
-# Kick command (for admins)
+# Create a poll
 @bot.command()
-@commands.has_permissions(kick_members=True)
-async def kick(ctx, member: discord.Member, *, reason=None):
-    await member.kick(reason=reason)
-    await ctx.send(f"{member.name} has been kicked. Reason: {reason}")
+async def poll(ctx, question: str, *options):
+    if len(options) < 2:
+        await ctx.send("You need at least two options to create a poll!")
+        return
+    if len(options) > 10:
+        await ctx.send("You can have a maximum of 10 options.")
+        return
+
+    embed = discord.Embed(title="Poll", description=question, color=discord.Color.green())
+    emoji_numbers = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣", "🔟"]
+
+    for i, option in enumerate(options):
+        embed.add_field(name=f"{emoji_numbers[i]} {option}", value="\u200b", inline=False)
+
+    poll_message = await ctx.send(embed=embed)
+    for i in range(len(options)):
+        await poll_message.add_reaction(emoji_numbers[i])
 
 
-# Ban command (for admins)
+# Set a reminder
 @bot.command()
-@commands.has_permissions(ban_members=True)
-async def ban(ctx, member: discord.Member, *, reason=None):
-    await member.ban(reason=reason)
-    await ctx.send(f"{member.name} has been banned. Reason: {reason}")
+async def remind(ctx, time: int, unit: str, *, reminder_text: str):
+    if unit not in ["seconds", "minutes", "hours"]:
+        await ctx.send("Invalid time unit! Use 'seconds', 'minutes', or 'hours'.")
+        return
+
+    remind_at = datetime.now()
+    if unit == "seconds":
+        remind_at += timedelta(seconds=time)
+    elif unit == "minutes":
+        remind_at += timedelta(minutes=time)
+    elif unit == "hours":
+        remind_at += timedelta(hours=time)
+
+    cursor.execute("""
+    INSERT INTO reminders (user_id, username, reminder_text, remind_at)
+    VALUES (?, ?, ?, ?)
+    """, (str(ctx.author.id), ctx.author.name, reminder_text, str(remind_at)))
+    conn.commit()
+
+    await ctx.send(f"Reminder set! I'll remind you in {time} {unit}.")
 
 
-# Fetch messages from the database
-@bot.command()
-async def fetch_logs(ctx, limit: int = 10):
-    cursor.execute("SELECT username, message_content, timestamp FROM messages ORDER BY id DESC LIMIT ?", (limit,))
-    rows = cursor.fetchall()
-    logs = "\n".join([f"[{row[2]}] {row[0]}: {row[1]}" for row in rows])
-    await ctx.send(f"**Last {limit} Messages:**\n{logs}")
+# Task to check for reminders
+@tasks.loop(seconds=30)
+async def check_reminders():
+    now = datetime.now()
+    cursor.execute("SELECT id, user_id, reminder_text FROM reminders WHERE remind_at <= ?", (str(now),))
+    reminders = cursor.fetchall()
 
-
-# Run the bot (replace with your bot token)
-bot.run("YOUR_BOT_TOKEN")
+    for reminder in reminders:
+        user = bot.get_user(int(reminder[1]))
+        if user:
+            await user.send(f"⏰ Reminder: {reminder[2]}")
+        cursor.execute("DELETE FROM reminders WHERE id = ?", (reminder[0],))
+        conn.commit()
